25aebf8ddd711bedb55bedb360578274fcb1dff0
David Wilson <daviwil@github.com>
2018-06-21 22:07:27 -0700

    Add node-gyp parameter --build_v8_with_gn=false
    More info: 
    https://github.com/nodejs/node-gyp/issues/1457#issuecomment-395490546

--- scripts/install.js
+++ scripts/install.js
@@ -4,7 +4,7 @@ const os = require('os');
 const path = require('path');
 const spawn = require('child_process').spawn;
 
-const p = spawn(os.platform() === 'win32' ? 'node-gyp.cmd' : 'node-gyp', ['rebuild'], {
+const p = spawn(os.platform() === 'win32' ? 'node-gyp.cmd' : 'node-gyp', ['rebuild', '--build_v8_with_gn=false'], {
   cwd: path.join(__dirname, '..'),
   stdio: 'inherit'
 });
